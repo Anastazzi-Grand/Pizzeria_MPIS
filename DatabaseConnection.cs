d7ef3b76496807f06b3df9edd0c1a82f01c4aa44
Nastya <vnedostupa@yandex.ru>
2024-06-01 12:09:32 +0300

    Добавьте файлы проекта.

---
+++ DatabaseConnection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace Pizzeria
+{
+    internal class DatabaseConnection
+    {
+        private static string connectionString = "Server=localhost;Port=5432;Database=postgres;User Id=postgres;Password=nasa;";
+
+        public static NpgsqlConnection GetConnection()
+        {
+            NpgsqlConnection conn = new NpgsqlConnection(connectionString);
+            try
+            {
+                conn.Open();
+                return conn;
+
+            } catch (NpgsqlException ex)
+            {
+                Console.WriteLine($"Ошибка при подключении к базе данных: {ex.Message}");
+                throw;
+            }
+        }
+    }
+}
